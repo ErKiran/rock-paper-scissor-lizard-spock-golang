50428a3b4b70f32942d4bdc30117611851623e8c
Kiran Adhikari <kiruu1238@gmail.com>
2021-06-13 22:06:35 +0545

    init

---
+++ api/controllers/routes.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+	"rps/api/middlewares"
+
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	Router *mux.Router
+}
+
+func (server *Server) setJSON(path string, next func(http.ResponseWriter, *http.Request), method string) {
+	server.Router.HandleFunc(path, middlewares.SetMiddlewareJSON(next)).Methods(method, "OPTIONS")
+}
+func (server *Server) initializeRoutes() {
+	server.setJSON("/health", server.Health, http.MethodGet)
+	server.setJSON("/choices", server.Choices, http.MethodGet)
+	server.setJSON("/choice", server.Choice, http.MethodGet)
+	server.setJSON("/play", server.Play, http.MethodPost)
+}
+
+func (server *Server) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
